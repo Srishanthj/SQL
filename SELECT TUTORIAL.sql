58f6330e1740c80127ac5a0370ae3ac310006051
Srishanth J <114941141+Srishanthj@users.noreply.github.com>
2024-07-31 18:10:48 +0530

    Add files via upload

---
+++ SELECT TUTORIAL.sql
@@ -0,0 +1,10 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+SELECT 
+first_name,
+last_name,
+age ,
+(age + 10) * 10
+FROM parks_and_recreation.employee_demographics;
+# PEMDAS 
+SELECT DISTINCT first_name,gender
+FROM parks_and_recreation.employee_demographics;
